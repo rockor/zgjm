0a186035dcadb08d764ec81257cfbf589efc49c6
wang <wangzhanghe@gm825.com>
2018-08-12 15:21:44 +0800

    修改东西

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//app.js
-<<<<<<< HEAD
+
 const App = require('./utils/ald-stat.js').App;
 const xampp = 
     require('./utils/xmadx_sdk.min.js').xmad(App,'App').xmApp;
@@ -8,10 +7,4 @@ App({
     xampp,
 })
 
-=======
-// var App = require('./utils/xmadx_sdk.min.js').xmad(App,'App').xmApp;
 
-const App = require('./utils/ald-stat.js').App;
-
-console.log(App)
->>>>>>> 8c1e3bb676a73f9feffd2477538f4a91ec96eccc
